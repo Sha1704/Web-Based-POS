09a708e2f1784841c49f988dba3cadd1fb7d8c79
AzulAldama <98558900+AzulAldama@users.noreply.github.com>
2025-11-18 19:21:41 -0600

    Sprint 3
    finished add_to_inventory

--- Backend/inventory.py
+++ Backend/inventory.py
@@ -3,8 +3,25 @@ class Inventory:
     def track_inventory(self): # Dariya
         pass
 
-    def add_to_inventory(self): # Azul
-        pass
+    def add_to_inventory(self, name, price, quantity, category): # Azul
+        #check to see if item already exists
+        try:
+            query = "SELECT item_id FROM inventory_item WHERE item_name = %s;"
+            exist = backend.run_query(query, (name,))
+            if exist:
+                print(f"Item '{name}' already exists in inventory.")
+                return False
+            
+            #add to the database
+            insertQuery = 'INSERT INTO inventory_item (item_name, price, quantity, category) VALUES (%s, %s, %s, %s)'
+            backend.run_query(insertQuery, (name, price, quantity, category))
+            print(f"Added successfully")
+            return True
+
+        except Exception as e:
+            print(f"Error adding item to SQL: {e}")
+            return False
+
 
     def update_product_count(self): # Shalom
         pass
